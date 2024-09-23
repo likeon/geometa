d41c6e5327e95bc83e487702808bbf80e6011858
Aleksandr Sterkhov <github@so.likeon.dev>
2024-09-23 19:25:24 +0200

    analytics only in prod

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script>
 	import '../app.css';
 	import NavBar from '$lib/components/NavBar.svelte';
+  import { dev } from '$app/environment';
 </script>
+
+<svelte:head>
+  {#if !dev}
+    <script defer src="https://cloud.umami.is/script.js" data-website-id="3cb09286-f8f9-43c8-85ee-8df2b972c113"></script>
+  {/if}
+</svelte:head>
 
 <div class="app">
 	<NavBar />
