962db5f44c49b2adc4d6443f85421f43f1151d57
Aleksandr Sterkhov <github@so.likeon.dev>
2024-10-07 21:55:30 +0200

    added groups list

--- src/routes/(admin)/dev/dash/+page.server.ts
+++ src/routes/(admin)/dev/dash/+page.server.ts
@@ -1,5 +1,18 @@
-import { redirect } from '@sveltejs/kit';
+import { db } from '$lib/drizzle';
+import { mapGroupPermissions, mapGroups } from '$lib/db/schema';
+import { eq } from 'drizzle-orm';
+import { error } from '@sveltejs/kit';
 
-export function load() {
-  throw redirect(307, '/dev/dash/groups/1');
-}
+export const load = async ({ locals }) => {
+  if (!locals.user?.id) {
+    error(403, 'Permission denied');
+  }
+
+  const groups = await db
+    .select()
+    .from(mapGroups)
+    .innerJoin(mapGroupPermissions, eq(mapGroupPermissions.mapGroupId, mapGroups.id))
+    .where(eq(mapGroupPermissions.userId, locals.user.id));
+
+  return { groups };
+};
---
+++ src/routes/(admin)/dev/dash/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let data;
+</script>
+
+<div class="container">
+  <ul class="list-disc">
+    {#each data.groups as group (group.map_groups.id)}
+      <li>
+        <a href="/dev/dash/groups/{group.map_groups.id}">{group.map_groups.name}</a>
+      </li>
+    {/each}
+  </ul>
+</div>
