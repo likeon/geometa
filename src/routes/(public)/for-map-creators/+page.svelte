6219d435cf9e513229dcb57f8fb364fc866dc8d8
Aleksandr Sterkhov <github@so.likeon.dev>
2024-10-28 18:02:22 +0100

    temporary removed dashbord link

--- src/routes/(public)/for-map-creators/+page.svelte
+++ src/routes/(public)/for-map-creators/+page.svelte
@@ -67,12 +67,14 @@
       </span>
     </li>
   </ul>
-
-  <div class="mt-3 w-full flex justify-center">
-    <GradientButton href="/dev/dash" color="greenToBlue"
-      ><Icon icon="gis:poi-map" color="white" class="mr-3 h-4 w-4" />Open Creator Dashboard</GradientButton>
-  </div>
-  <div class="w-full flex justify-center mb-10 mt-2">
-    <p class="text-sm">Requires login with Discord</p>
-  </div>
+  <p class="mt-3 mb-10">
+    Drop a message in <strong>#help-desk</strong> channel in our Discord to sign up.
+  </p>
+  <!--  <div class="mt-3 w-full flex justify-center">-->
+  <!--    <GradientButton href="/dev/dash" color="greenToBlue"-->
+  <!--      ><Icon icon="gis:poi-map" color="white" class="mr-3 h-4 w-4" />Open Creator Dashboard</GradientButton>-->
+  <!--  </div>-->
+  <!--  <div class="w-full flex justify-center mb-10 mt-2">-->
+  <!--    <p class="text-sm">Requires login with Discord</p>-->
+  <!--  </div>-->
 </div>
