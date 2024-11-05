36eea4bdb6bcd70c337db61b21f4869652ea2507
Aleksandr Sterkhov <github@so.likeon.dev>
2024-11-05 14:50:12 +0100

    admin front page: added list of all groups for superusers

--- src/routes/(admin)/dev/dash/+page.server.ts
+++ src/routes/(admin)/dev/dash/+page.server.ts
@@ -1,6 +1,6 @@
 import { db } from '$lib/drizzle';
-import { mapGroupPermissions, mapGroups } from '$lib/db/schema';
-import { eq } from 'drizzle-orm';
+import { mapGroupLocations, mapGroupPermissions, mapGroups, users } from '$lib/db/schema';
+import { desc, eq, sql } from 'drizzle-orm';
 import { error } from '@sveltejs/kit';
 
 export const prerender = false;
@@ -10,11 +10,32 @@ export const load = async ({ locals }) => {
     error(403, 'Permission denied');
   }
 
-  const groups = await db
+  const userGroups = await db
     .select()
     .from(mapGroups)
     .innerJoin(mapGroupPermissions, eq(mapGroupPermissions.mapGroupId, mapGroups.id))
     .where(eq(mapGroupPermissions.userId, locals.user.id));
 
-  return { groups };
+  const user = await db.query.users.findFirst({ where: eq(users.id, locals.user.id) });
+  let allGroups;
+  if (user?.isSuperadmin) {
+    allGroups = await db
+      .select({
+        id: mapGroups.id,
+        name: mapGroups.name,
+        authors: sql<string | null>`
+          (SELECT GROUP_CONCAT(u.username)
+           FROM map_group_permissions mgp
+           JOIN user u ON u.id = mgp.user_id
+           WHERE mgp.map_group_id = "map_groups"."id")`,
+        locationCount: sql<number>`count(${mapGroupLocations.id})`.mapWith(Number)
+      })
+      .from(mapGroups)
+      .leftJoin(mapGroupLocations, eq(mapGroups.id, mapGroupLocations.mapGroupId))
+      .groupBy(mapGroups.id)
+      .orderBy(desc(sql<number>`count(${mapGroupLocations.id})`));
+  } else {
+    allGroups = null;
+  }
+  return { userGroups, allGroups };
 };
--- src/routes/(admin)/dev/dash/+page.svelte
+++ src/routes/(admin)/dev/dash/+page.svelte
@@ -4,10 +4,21 @@
 
 <div class="container">
   <ul class="list-disc">
-    {#each data.groups as group (group.map_groups.id)}
+    {#each data.userGroups as group (group.map_groups.id)}
       <li>
         <a href="/dev/dash/groups/{group.map_groups.id}">{group.map_groups.name}</a>
       </li>
     {/each}
   </ul>
+  {#if data.allGroups}
+    <hr />
+    <ul class="list-disc">
+      {#each data.allGroups as group (group.id)}
+        <li>
+          <a href="/dev/dash/groups/{group.id}"
+            >{group.name}, locations: {group.locationCount} ({group.authors})</a>
+        </li>
+      {/each}
+    </ul>
+  {/if}
 </div>
