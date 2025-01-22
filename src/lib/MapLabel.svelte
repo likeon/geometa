2a1d99df3f926d32e5933cee31b0033bc5156261
Aleksandr Sterkhov <github@so.likeon.dev>
2025-01-22 16:02:51 +0100

    moved map label to a component

---
+++ src/lib/MapLabel.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  function navigate() {
+    const mapId = window.location.pathname.split('/maps/')[1]; // Extract map ID from URL
+    window.open(`https://learnablemeta.com/maps/${mapId}`, '_blank'); // Open the meta list in a new tab
+  }
+</script>
+
+<div class="container">
+  <p>LearnableMeta Enabled</p>
+  <button onclick={navigate}>Meta List</button>
+</div>
+
+<style>
+  .container {
+    background-color: #252541;
+    color: #fff;
+    text-align: center;
+    z-index: 999999;
+    position: absolute;
+    bottom: -1px;
+    left: 0;
+    width: 220px;
+    height: 50%;
+    box-sizing: border-box;
+    border-radius: 0 0 110px 110px;
+    clip-path: inset(40% 0 0 0);
+  }
+
+  p {
+    font-size: 14px;
+    margin-top: 48px;
+    font-weight: bold;
+  }
+
+  button {
+    margin-top: 6px;
+    padding: 6px 12px;
+    font-size: 12px;
+    color: #fff;
+    background-color: #4CAF50;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+</style>
