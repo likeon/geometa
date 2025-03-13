2db10191f26d9dacf25fb62f2db8ecca9e975e50
Aleksandr Sterkhov <github@so.likeon.dev>
2025-03-13 22:18:57 +0100

    postgres: added optional request logging

--- src/lib/drizzle.ts
+++ src/lib/drizzle.ts
@@ -2,9 +2,10 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import { DATABASE_URL } from '$env/static/private';
 import * as schema from '$lib/db/schema';
+import { env } from '$env/dynamic/private';
 
-export function getDb(env: App.Platform['env']) {
-  const client = postgres(env.db?.connectionString || DATABASE_URL, { prepare: false });
-  return drizzle(client, { schema });
+export function getDb(platform_env: App.Platform['env']) {
+  const client = postgres(platform_env.db?.connectionString || DATABASE_URL);
+  return drizzle(client, { schema, logger: env.DRIZZLE_LOGGING === 'true' || false });
 }
 export type DB = ReturnType<typeof getDb>;
