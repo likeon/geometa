338733ea12ffe41569a4eeaec39d2d7633f9ff24
Aleksandr Sterkhov <github@so.likeon.dev>
2025-03-14 18:29:51 +0100

    postgres: added pool client for local development

--- src/lib/drizzle.ts
+++ src/lib/drizzle.ts
@@ -2,10 +2,15 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import { DATABASE_URL } from '$env/static/private';
 import * as schema from '$lib/db/schema';
-import { env } from '$env/dynamic/private';
+import { dev } from '$app/environment';
+
+let poolClient: postgres.Sql;
+if (dev) {
+  poolClient = postgres(DATABASE_URL);
+}
 
 export function getDb(platform_env: App.Platform['env']) {
-  const client = postgres(platform_env.db?.connectionString || DATABASE_URL);
+  const client = poolClient || postgres(platform_env.db?.connectionString || DATABASE_URL);
   return drizzle(client, { schema, logger: false });
 }
 export type DB = ReturnType<typeof getDb>;
