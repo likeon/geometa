f6492d0f320c46c0cc13e5f30f1b3d232448a46d
Aleksandr Sterkhov <github@so.likeon.dev>
2025-03-14 15:25:27 +0100

    postgres: added map location change trigger

---
+++ migrations/0009_wealthy_pet_avengers.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE FUNCTION map_group_location_update_modified()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF row(NEW.*) IS DISTINCT FROM row(OLD.*) THEN
+        NEW.modified_at = EXTRACT(EPOCH FROM NOW())::int;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_map_group_location_update_modified
+BEFORE UPDATE ON map_group_locations
+FOR EACH ROW
+WHEN (OLD IS DISTINCT FROM NEW)
+EXECUTE FUNCTION map_group_location_update_modified();
