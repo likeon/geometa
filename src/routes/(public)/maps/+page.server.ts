93a5da0e173ef8b60350c0804f21cea0c504a3e1
Aleksandr Sterkhov <github@so.likeon.dev>
2025-03-18 19:53:47 +0100

    removed maps page redis caching, removed upstash dependency

--- src/routes/(public)/maps/+page.server.ts
+++ src/routes/(public)/maps/+page.server.ts
@@ -1,39 +1,7 @@
-import { asc, desc, eq, type InferSelectModel, sql } from 'drizzle-orm';
+import { asc, desc, eq, sql } from 'drizzle-orm';
 import { maps, regions } from '$lib/db/schema';
-import { Redis } from '@upstash/redis/cloudflare';
-import * as privateEnv from '$env/static/private';
-
-let redis: Redis;
-if (privateEnv.REDIS_TOKEN) {
-  redis = new Redis({
-    url: 'https://prepared-flea-35486.upstash.io',
-    token: privateEnv.REDIS_TOKEN
-  });
-}
-
-const mapCacheKey = `${privateEnv.REDIS_KEY_PREFIX ? privateEnv.REDIS_KEY_PREFIX : 'prod'}:query:maps:allmaps`;
-const regionsKey = `${privateEnv.REDIS_KEY_PREFIX ? privateEnv.REDIS_KEY_PREFIX : 'prod'}:query:maps:regions`;
-const contentCacheEnabled = true;
-
-type Region = InferSelectModel<typeof regions>;
-type Map = InferSelectModel<typeof maps> & {
-  locationsCount: number;
-  regions: string;
-  metasCount: number;
-};
 
 export const load = async (event) => {
-  if (contentCacheEnabled && redis) {
-    const allMapsPromise = redis.get(mapCacheKey);
-    const regionsPromise = redis.get(regionsKey);
-    const [allMapsCache, regionsListCache] = await Promise.all([allMapsPromise, regionsPromise]);
-    if (allMapsCache && regionsListCache) {
-      return {
-        allMaps: allMapsCache as Map[],
-        regionsList: regionsListCache as Region[]
-      };
-    }
-  }
   const regionsList = await event.locals.db.select().from(regions).orderBy(asc(regions.ordering));
   const allMaps = await event.locals.db.query.maps.findMany({
     extras: {
@@ -55,12 +23,5 @@ export const load = async (event) => {
       sql`COALESCE(maps.number_of_games_played_diminished, 0) + FLOOR(RANDOM() * 41) DESC`
     ]
   });
-
-  if (contentCacheEnabled && redis) {
-    await Promise.all([
-      redis.set(mapCacheKey, JSON.stringify(allMaps), { ex: 3600 }),
-      redis.set(regionsKey, JSON.stringify(regionsList), { ex: 3600 })
-    ]);
-  }
   return { allMaps, regionsList };
 };
