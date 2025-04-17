b15192d6441af92def5d0b76bc6c8ce267037652
Aleksandr Sterkhov <github@so.likeon.dev>
2025-04-17 20:22:09 +0200

    disabled sentry in dev mode

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,58 +4,64 @@ import { type Handle, redirect } from '@sveltejs/kit';
 import { initializeLucia } from '$lib/auth';
 import { getDb } from '$lib/drizzle';
 
-export const handle: Handle = sequence(
-  initCloudflareSentryHandle({
-    dsn: 'https://e3246bfc0996fb9eb5ced91d84b98301@o4506021108383744.ingest.us.sentry.io/4508975174385664',
-    tracesSampleRate: 1.0
-  }),
-  sentryHandle(),
-  async ({ event, resolve }) => {
-    const db = getDb(event.platform!.env);
-    const lucia = initializeLucia(db);
-    event.locals.db = db;
-    event.locals.lucia = lucia;
+const dev = process.env.NODE_ENV === 'development';
 
-    const isAdminUrl = event.url.pathname.startsWith('/dev/dash');
-    let redirectToLogin = isAdminUrl;
+// Only use Sentry in production
+const sentryHandlers = dev
+  ? []
+  : [
+      initCloudflareSentryHandle({
+        dsn: 'https://e3246bfc0996fb9eb5ced91d84b98301@o4506021108383744.ingest.us.sentry.io/4508975174385664',
+        tracesSampleRate: 1.0
+      }),
+      sentryHandle()
+    ];
 
-    const sessionId = event.cookies.get(lucia.sessionCookieName);
-    if (!sessionId) {
-      console.log('No session');
-      event.locals.user = null;
-      event.locals.session = null;
-    } else {
-      const { session, user } = await lucia.validateSession(sessionId);
-      if (session) {
-        console.log('Session is valid');
-        if (session.fresh) {
-          console.log('Session needs refresh');
-          // session cookie is valid, but needs a refresh
-          const sessionCookie = lucia.createSessionCookie(session.id);
-          event.cookies.set(sessionCookie.name, sessionCookie.value, {
-            path: '/',
-            ...sessionCookie.attributes
-          });
-        }
-        redirectToLogin = false;
-      }
-      if (!session) {
-        console.log('Session is invalid');
-        const sessionCookie = lucia.createBlankSessionCookie();
+export const handle: Handle = sequence(...sentryHandlers, async ({ event, resolve }) => {
+  const db = getDb(event.platform!.env);
+  const lucia = initializeLucia(db);
+  event.locals.db = db;
+  event.locals.lucia = lucia;
+
+  const isAdminUrl = event.url.pathname.startsWith('/dev/dash');
+  let redirectToLogin = isAdminUrl;
+
+  const sessionId = event.cookies.get(lucia.sessionCookieName);
+  if (!sessionId) {
+    console.log('No session');
+    event.locals.user = null;
+    event.locals.session = null;
+  } else {
+    const { session, user } = await lucia.validateSession(sessionId);
+    if (session) {
+      console.log('Session is valid');
+      if (session.fresh) {
+        console.log('Session needs refresh');
+        // session cookie is valid, but needs a refresh
+        const sessionCookie = lucia.createSessionCookie(session.id);
         event.cookies.set(sessionCookie.name, sessionCookie.value, {
           path: '/',
           ...sessionCookie.attributes
         });
       }
-      event.locals.user = user;
-      event.locals.session = session;
+      redirectToLogin = false;
     }
-
-    if (redirectToLogin) {
-      throw redirect(302, '/login');
+    if (!session) {
+      console.log('Session is invalid');
+      const sessionCookie = lucia.createBlankSessionCookie();
+      event.cookies.set(sessionCookie.name, sessionCookie.value, {
+        path: '/',
+        ...sessionCookie.attributes
+      });
     }
-
-    return resolve(event);
+    event.locals.user = user;
+    event.locals.session = session;
   }
-);
-export const handleError = handleErrorWithSentry();
+
+  if (redirectToLogin) {
+    throw redirect(302, '/login');
+  }
+
+  return resolve(event);
+});
+export const handleError = dev ? undefined : handleErrorWithSentry();
