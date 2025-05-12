1004f35370d9ca0f8cdd046eadcd1131be1f2b26
Aleksandr Sterkhov <github@so.likeon.dev>
2025-05-12 14:49:28 +0200

    api: fixed image build, removed ingres

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,5 +3,6 @@ import { publicRouter } from '@/routes/public';
 
 const app = new Elysia({prefix: '/api'})
   .use(publicRouter)
+  .get('/health-check', () => {return 'ok'})
   .listen(3000);
 export type App = typeof app;
