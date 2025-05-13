53781ca1af647bb5ef33914ba13fa6ed23297df3
Aleksandr Sterkhov <github@so.likeon.dev>
2025-05-13 12:42:32 +0200

    frontend: treat env.API_HOST as private variable

--- apps/frontend/src/lib/api.ts
+++ apps/frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 import { edenTreaty } from '@elysiajs/eden';
 import type { App } from '$api/index.js';
-import { env } from '$env/dynamic/public';
+import { env } from '$env/dynamic/private';
 
 const apiHost = env.API_HOST || 'localhost:3000';
 export const api = edenTreaty<App>(`http://${apiHost}`).api;
