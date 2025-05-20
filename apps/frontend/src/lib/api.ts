72f398c49289ddc34dc5b2ed69e6b5a748fbf439
Jacek Kurczych <lonanche@gmail.com>
2025-05-20 07:44:28 +0100

    frontend: added bearer token to api calls for internal route

--- apps/frontend/src/lib/api.ts
+++ apps/frontend/src/lib/api.ts
@@ -1,6 +1,15 @@
 import { treaty } from '@elysiajs/eden';
 import type { App } from '$api';
 import { env } from '$env/dynamic/private';
+const frontendToken = process.env.FRONTEND_API_TOKEN;
+const prod = process.env.NODE_ENV === 'production';
 
 const apiHost = env.API_HOST || 'localhost:3000';
-export const api = treaty<App>(`http://${apiHost}`).api;
+export const api = treaty<App>(`http://${apiHost}`, {
+  headers(path) {
+    if (prod && frontendToken && path.startsWith('/api/internal'))
+      return {
+        authorization: `Bearer ${frontendToken}`
+      };
+  }
+}).api;
