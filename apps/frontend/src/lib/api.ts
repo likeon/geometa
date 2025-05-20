0a94a7ef96d44151b34e62cbc6208c7fb1eff99c
Jacek Kurczych <lonanche@gmail.com>
2025-05-20 12:51:36 +0100

    frontend: fixed env var imports

--- apps/frontend/src/lib/api.ts
+++ apps/frontend/src/lib/api.ts
@@ -1,8 +1,8 @@
 import { treaty } from '@elysiajs/eden';
 import type { App } from '$api';
 import { env } from '$env/dynamic/private';
-const frontendToken = process.env.FRONTEND_API_TOKEN;
-const prod = process.env.NODE_ENV === 'production';
+const frontendToken = env.FRONTEND_API_TOKEN;
+const prod = env.NODE_ENV === 'production';
 
 const apiHost = env.API_HOST || 'localhost:3000';
 export const api = treaty<App>(`http://${apiHost}`, {
