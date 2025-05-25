431bd3b1f230a2f2aac4090328369415db380d93
Aleksandr Sterkhov <github@so.likeon.dev>
2025-05-25 18:47:40 +0200

    turned down map label glow

--- src/lib/components/MapLabel.svelte
+++ src/lib/components/MapLabel.svelte
@@ -1,29 +1,32 @@
 <script lang="ts">
-  let { mapId }: {mapId: string} = $props();
+  let { mapId }: { mapId: string } = $props();
 </script>
 
 <div class="geometa-map-label-container">
   <p>LearnableMeta Enabled</p>
-  <a href={`https://learnablemeta.com/maps/${mapId}`} target="_blank"><button>Meta List</button></a>
+  <a href={`https://learnablemeta.com/maps/${mapId}`} target="_blank">
+    <button>Meta List</button>
+  </a>
 </div>
 
 <style>
   .geometa-map-label-container {
     background-color: #0003;
-  color: #fff;
-  text-align: center;
-  z-index: 999999;
-  position: absolute;
-  bottom: 4px;
-  right: 4px;
-  box-sizing: border-box;
-  border-radius: 8px;
-  padding: 8px;
-  backdrop-filter: blur(8px);
-  display: flex;
-  align-items: center;
-  gap: 8px;
-  }
+    color: #fff;
+    text-align: center;
+    z-index: 999999;
+    position: absolute;
+    bottom: 4px;
+    right: 4px;
+    box-sizing: border-box;
+    border-radius: 8px;
+    padding: 8px;
+    backdrop-filter: blur(8px);
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    text-shadow: 0 0 10px rgba(255, 255, 255, 0.1);
+}
 
   p {
     font-size: 14px;
