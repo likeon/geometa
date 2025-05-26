4d1d4f4d79de4151eb45e239e5e2b8649f47b228
Jacek Kurczych <lonanche@gmail.com>
2025-05-26 09:36:54 +0100

    frontend: added information how to add metas

--- apps/frontend/src/routes/(admin)/dev/dash/personal/[id]/+page.svelte
+++ apps/frontend/src/routes/(admin)/dev/dash/personal/[id]/+page.svelte
@@ -2,7 +2,7 @@
   import PersonalNavBar from '$lib/components/PersonalNavBar.svelte';
   import { columns } from './columns';
    import DataTable from './table.svelte';
-   
+
  let { data } = $props();
 
 
@@ -12,6 +12,18 @@
 
 <PersonalNavBar mapName={data.mapName} id={data.id}/>
   <div class="mt-5">
+    {#if data.metaList.length === 0}
+
+      <p>
+        To add metas, go to the meta list of a map with sharing enabled, select the metas you want, and add them to your map.
+        Visit <a
+        target="_blank"
+        href='/maps'
+        class="font-semibold underline hover:text-blue-800 dark:hover:text-blue-900"
+      >maps page</a> and enable the "Shared" filter to see which maps are available for selecting metas!
+      </p>
+      {:else}
  <DataTable data={data.metaList} {columns}/>
+      {/if}
   </div>
 
