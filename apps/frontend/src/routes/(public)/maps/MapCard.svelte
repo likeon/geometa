d62dc02bc29188df7f7713ec3fd907119bcff321
Aleksandr Sterkhov <github@so.likeon.dev>
2025-05-29 01:03:08 +0200

    frontend: MapCard adjusted shared text color in light mode

--- apps/frontend/src/routes/(public)/maps/MapCard.svelte
+++ apps/frontend/src/routes/(public)/maps/MapCard.svelte
@@ -14,7 +14,7 @@
   let {
     map
   }: {
-    map: PageData['allMaps'][number];
+    map: Awaited<PageData['allMaps']>[number];
   } = $props();
 
   const difficulties: Record<
@@ -49,7 +49,7 @@
       {#if map.isShared}
         <div>
           <Badge
-            class="bg-amber-500 text-black dark:bg-amber-400 dark:text-amber-950 font-semibold px-2.5 py-0.5 rounded-md">
+            class="bg-amber-500 text-amber-900 dark:bg-amber-400 dark:text-amber-950 font-semibold px-2.5 py-0.5 rounded-md">
             <RaStarlineIcon class="inline-block mr-1" />
             Shared
           </Badge>
