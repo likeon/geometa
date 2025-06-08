fc089878631fd5695922683a523843310f779531
Jacek Kurczych <lonanche@gmail.com>
2025-06-08 12:51:56 +0100

    bumped userscript version

--- apps/api/src/routes/userscript.ts
+++ apps/api/src/routes/userscript.ts
@@ -13,7 +13,7 @@ import { generateFooter } from '@api/lib/userscript/utils';
 import { eq, sql } from 'drizzle-orm';
 import { Elysia, t } from 'elysia';
 
-const userscriptVersion = '0.86';
+const userscriptVersion = '0.87';
 
 const mapInfoQuery = db.query.maps
   .findFirst({
